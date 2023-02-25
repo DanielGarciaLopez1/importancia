ab3a7a8aba3a994d21ea793217cf0bbe12c014e5
Daniel <72498622+DanielGarciaLopez1@users.noreply.github.com>
2023-02-25 11:54:17 +0100

    Add files via upload

---
+++ lanzador.sh
@@ -0,0 +1,4 @@
+python /home/daniel_garcia/importancia/importancia_argentina.py & 
+python /home/daniel_garcia/importancia/importancia_canada.py &
+python /home/daniel_garcia/importancia/importancia_ireland.py &
+python /home/daniel_garcia/importancia/importancia_usa.py &
